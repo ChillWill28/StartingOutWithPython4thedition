5912dfd41814987f3cc64844d2bdfd01c4459e0f
ChillWill28 <79624542+ChillWill28@users.noreply.github.com>
2021-04-13 17:32:03 -0500

    Add files via upload

---
+++ acme_dryer.py
@@ -0,0 +1,38 @@
+def main():
+    startup_message()
+    step_1()
+    step_2()
+    step_3()
+    step_4()
+
+
+def startup_message():
+    print('This is a message to describe')
+    print('the steps of involved in')
+    print('repairing the ACME 3000 dryer.')
+    input('Press Enter to see Step 1.')
+
+def step_1():
+    print('1. Unplug the dryer and move it away from the wall.')
+    print()
+    input('Press Enter to see Step 2.')
+
+def step_2():
+    print('2. Remove the six screws from the back of the dryer.')
+    print()
+    input('Press Enter to see Step 3.')
+
+def step_3():
+    print("3. Remove the dryer's back panel.")
+    print()
+    input('Press Enter to see Step 4.')
+
+def step_4():
+    print('4. Pull the top of the dryer straight up.')
+    print()
+    
+    
+
+main()
+
+
